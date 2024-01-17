a2e8e83e07bc2ab180b02b72f51d119a823ce466
“Alastair <66482739+alastairsounds@users.noreply.github.com>
2024-01-17 12:14:23 -0700

    init

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <!-- create a search bar -->
+    <!-- the search bar takes in a movie name, and passes the name as a string to /movies?search={title} -->
+    <h3>Search for a movie</h3>
+    <form action="/movies" method="get">
+      <input type="text" name="search" placeholder="Search for a movie" />
+      <button type="submit">Search</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
